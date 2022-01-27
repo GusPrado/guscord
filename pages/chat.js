e78be6064df89b2bcec2e1e6a8db25e22dff3ead
Gus Prado <49629739+GusPrado@users.noreply.github.com>
2022-01-26 21:14:04 -0300

    chore(*): Add Chat page and default Next App

---
+++ pages/chat.js
@@ -0,0 +1,7 @@
+export default function ChatPage() {
+  return (
+    <div>
+      <h1>Chat here!!!</h1>
+    </div>
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -1,36 +1,7 @@
+import { useState } from 'react';
+import { useRouter } from 'next/router';
 import { Box, Button, Text, TextField, Image } from '@skynexui/components';
 import appConfig from '../config.json';
-
-function GlobalStyle() {
-  return (
-    <style global jsx>{`
-      * {
-        margin: 0;
-        padding: 0;
-        box-sizing: border-box;
-        list-style: none;
-      }
-      body {
-        font-family: 'Open Sans', sans-serif;
-      }
-      /* App fit Height */
-      html,
-      body,
-      #__next {
-        min-height: 100vh;
-        display: flex;
-        flex: 1;
-      }
-      #__next {
-        flex: 1;
-      }
-      #__next > * {
-        flex: 1;
-      }
-      /* ./App fit Height */
-    `}</style>
-  );
-}
 
 function Titulo(props) {
   const Tag = props.tag || 'h1';
@@ -61,11 +32,12 @@ function Titulo(props) {
 // export default HomePage;
 
 export default function PaginaInicial() {
-  const username = 'peas';
+  const [username, setUsername] = useState('');
+  const [minChar, setMinChar] = useState(false);
+  const router = useRouter();
 
   return (
     <>
-      <GlobalStyle />
       <Box
         styleSheet={{
           display: 'flex',
@@ -100,6 +72,10 @@ export default function PaginaInicial() {
           {/* Formulário */}
           <Box
             as='form'
+            onSubmit={(e) => {
+              e.preventDefault();
+              router.push('/chat');
+            }}
             styleSheet={{
               display: 'flex',
               flexDirection: 'column',
@@ -122,6 +98,16 @@ export default function PaginaInicial() {
             </Text>
 
             <TextField
+              value={username}
+              onChange={(e) => {
+                const valor = e.target.value;
+                if (valor.length > 2) {
+                  setMinChar(true);
+                } else {
+                  setMinChar(false);
+                }
+                setUsername(valor);
+              }}
               fullWidth
               textFieldColors={{
                 neutral: {
@@ -167,7 +153,11 @@ export default function PaginaInicial() {
                 borderRadius: '50%',
                 marginBottom: '16px',
               }}
-              src={`https://github.com/${username}.png`}
+              src={
+                minChar
+                  ? `https://github.com/${username}.png`
+                  : 'images/blank.png'
+              }
             />
             <Text
               variant='body4'
